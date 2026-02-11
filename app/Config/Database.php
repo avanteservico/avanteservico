aa64cb4036fb1f30cf692b3ab1cc2cf3716d3ce4
User <user@example.com>
2026-02-11 14:38:58 -0300

    fix: use corrected username variable in PDO connection

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,7 +26,7 @@ class Database
 
         try {
             $dsn = "pgsql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name . ";sslmode=require";
-            $this->conn = new PDO($dsn, $this->username, $this->password);
+            $this->conn = new PDO($dsn, $username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         } catch (PDOException $exception) {
